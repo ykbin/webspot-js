c5abccf8c519451879b5f539a2d6b80b476d55ad
Yurii Yakubin <yurii.yakubin@gmail.com>
2023-11-19 23:44:41 +0000

    postcss-build-utility

--- src/index.mjs
+++ src/index.mjs
@@ -5,6 +5,8 @@ import postcssMinify from '@csstools/postcss-minify';
 import postcssGlobalData from '@csstools/postcss-global-data';
 import postcssCustomProperties from 'postcss-custom-properties';
 
-export function build({config, buildType, buildDir}) {
-  console.log(">>>", config);
-}
+export default {
+  build: (config) => {
+    console.log(">>>", config);
+  }
+};
