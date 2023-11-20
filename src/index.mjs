0d3cff400d20e464bd6a2313e6bcc6029c710a49
Yurii Yakubin <yurii.yakubin@gmail.com>
2023-11-20 00:09:09 +0000

    postcss-build-utility

--- src/index.mjs
+++ src/index.mjs
@@ -1,3 +1,7 @@
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from 'url';
+
 import postcss from 'postcss';
 import postcssImport from 'postcss-import';
 import autoPrefixer from 'autoprefixer';
@@ -5,8 +9,43 @@ import postcssMinify from '@csstools/postcss-minify';
 import postcssGlobalData from '@csstools/postcss-global-data';
 import postcssCustomProperties from 'postcss-custom-properties';
 
-export default {
-  build: (config) => {
-    console.log(">>>", config);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function buildStyle({style, buildType, binaryDir}) {
+  const stylePlugins = [
+    postcssImport,
+    autoPrefixer,
+    postcssGlobalData({
+      files: [
+        style.prop,
+      ],
+    }),
+    postcssCustomProperties({
+      preserve: false
+    }),
+  ];
+
+  if (buildType !== "Debug")
+    stylePlugins.push(postcssMinify);
+
+  for (const [ key, val ] of Object.entries(style.entry)) {
+    const styleFilepath = path.resolve(__dirname, val);
+    const style = fs.readFileSync(styleFilepath, "utf-8");
+    const filename = `${key}.bundle.css`;
+    const output = path.resolve(binaryDir, dist, filename);
+    postcss(stylePlugins).process(style).then(result => {
+      fs.writeFile(output, result.css, () => true);
+      console.log(`Generate ${filename} [postcss]`);
+    });
   }
+};
+
+function build(config) {
+  buildStyle(config);
+}
+
+export default {
+  buildStyle,
+  build,
 };
